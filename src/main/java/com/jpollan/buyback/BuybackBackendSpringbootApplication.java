34fc74aa04c478fa565f51c5d98fc440aa2bbc7c
Juan Pollan <jpollan@ESITSPF3CEHMF.>
2026-05-04 00:11:48 +0200

    chore: initialize spring boot backend with gradle

---
+++ src/main/java/com/jpollan/buyback/BuybackBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jpollan.buyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuybackBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuybackBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpollan/buyback/BuybackBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpollan.buyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuybackBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
